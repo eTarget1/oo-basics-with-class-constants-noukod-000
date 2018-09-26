94573d9c33eb99e9235c3c1cb76d1dce8d3eba41
clifford <clifford@noukod.com>
2018-09-26 05:59:00 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,4 +12,10 @@ GENRES = []
     puts "Flipping the page...wow, you read fast!"
   end
 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+
+
 end
